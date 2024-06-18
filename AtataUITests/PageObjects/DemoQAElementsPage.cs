928f54aaf50ccce33f58ed1a85f0a3637c8a5da4
AnnaYemets <ttannushka@gmail.com>
2024-06-18 18:26:10 +0300

    Adding project files

---
+++ AtataUITests/PageObjects/DemoQAElementsPage.cs
@@ -0,0 +1,27 @@
+﻿using _ = AtataUITests.PageObjects.DemoQAElementsPage;
+
+namespace AtataUITests.PageObjects
+{
+    [Url("/elements")]
+    public sealed class DemoQAElementsPage : DemoQAPage<_>
+    {
+        [FindById("item-0")]
+        public Clickable<DemoQATextBoxPage, _> TextBox { get; private set; }
+
+        [FindById("item-4")]
+        public Clickable<DemoQAButtonsPage, _> Buttons { get; private set; }
+
+        [FindById("item-1")]
+        public Clickable<DemoQACheckBoxPage, _> CheckBoxes { get; private set; }
+
+        public static DemoQATextBoxPage GoToTextBoxesPage()
+        {
+            return Go.To<_>().TextBox.ClickAndGo();
+        }
+
+        public static DemoQACheckBoxPage GoToCheckBoxesPage()
+        {
+            return Go.To<_>().CheckBoxes.ClickAndGo();
+        }
+    }
+}
---
+++ AtataUITests/UITestFixture.cs
@@ -0,0 +1,15 @@
+﻿namespace AtataUITests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class UITestFixture
+    {
+        [SetUp]
+        public void SetUp() =>
+            AtataContext.Configure().Build();
+
+        [TearDown]
+        public void TearDown() =>
+        AtataContext.Current?.Dispose();
+    }
+}
